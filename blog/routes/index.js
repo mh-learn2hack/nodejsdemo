f7ab7c844abdbde3959cab08f3d8bdb219ef1ce0
Kenneth Ozdowy <koinkers@localhost.localdomain>
2014-11-06 21:53:54 -0500

    backend done, frontend needs to be prettified

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -3,47 +3,40 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-/* GET Hello World page */
-router.get('/helloworld', function(req, res){
-  res.render('helloworld', {title: 'Hello World!'});
-});
-
-router.get('/userlist', function(req, res){
   var db = req.db;
-  var collection = db.get('usercollection');
+  var collection = db.get('posts');
   collection.find({},{}, function(e,docs){
-    res.render('userlist', {
-      "userlist" : docs,
-      title: "User List"
+    res.render('index', {
+      "posts" : docs,
+      title: "My Blog"
     });
   });
 });
 
-router.get('/newuser', function(req, res){
-  res.render('newuser', { title: 'Add New User' });
+router.get('/submit', function(req, res){
+  res.render('submit', {title : "Submit"});
 });
 
-router.post('/adduser', function(req, res){
+router.post('/addpost', function(req, res){
   var db = req.db;
 
-  var userName = req.body.username;
-  var userEmail = req.body.useremail;
+  var title = req.body.title;
+  var author = req.body.author;
+  var content = req.body.content;
 
-  var collection = db.get('usercollection');
+  var collection = db.get('posts');
 
   collection.insert({
-    "username" : userName,
-    "email" : userEmail
+    "title" : title,
+    "author" : author,
+    "content" : content
   }, function (err, doc){
     if (err) {
       res.send("There was a problem adding the information to the database.");
     }
     else {
-      res.location("userlist");
-      res.redirect("userlist");
+      res.location("/");
+      res.redirect("/");
     }
   });
 });
